6bbd08ce1042382fe2712eb1f25897381358a743
Park-Donghyeon <68741593+Park-Donghyeon@users.noreply.github.com>
2020-12-16 01:51:55 +0900

    Acmicpc java

---
+++ src/SortPractice/NumSort2.java
@@ -0,0 +1,41 @@
+package SortPractice;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class NumSort2 {
+	public static void main(String[] args) {
+		// 오름차순 정렬
+		
+		Scanner input = new Scanner(System.in);
+		
+		int n = input.nextInt();
+		
+		int originalArr[] = new int[n];
+		for (int i=0; i<n; i++) { // 오리지널 배열 생성
+			originalArr[i] = input.nextInt();
+		}
+		
+//		System.out.println(Arrays.toString(sortUp(originalArr)));
+		sortUp(originalArr);
+		
+		input.close();
+	}
+	
+	public static void sortUp(int[] oriArr) {
+		int tmp;
+		
+		for(int i=0; i<oriArr.length; i++) {
+			int minIdx = i;
+			for(int j=i; j<oriArr.length; j++) {
+				if(oriArr[minIdx]>oriArr[j]) {
+					minIdx = j;
+				}
+			}
+			tmp = oriArr[i];
+			oriArr[i] = oriArr[minIdx];
+			oriArr[minIdx] = tmp;
+			System.out.println(oriArr[i]);
+		}
+	}
+}
